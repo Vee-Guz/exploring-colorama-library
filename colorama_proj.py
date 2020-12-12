936a78b4d359c31e03fd6759d05083db1795d565
Vee-Guz <vero.guzman48612@gmail.com>
2020-12-11 23:42:39 -0800

    Option 1 is now working. Updated option_1 function with print statements using examples in colorama

--- colorama_proj.py
+++ colorama_proj.py
@@ -1,17 +1,36 @@
 
 # for this project, there will be options that will allow user to view different possible ways that colorama can be used.
 
+from colorama import Fore, Back, Style
+
 def start_colorama():
     # option variable will store a number (1 or 2) that will represent the option that will be printed out
-    option = int(intput("Please input 1 or 2? "))
+    option = int(input("Please input 1 or 2? "))
 
 
     # option 1 is an example of using colorama with text
     # option 2 is an example of using colorama in an artistic form
 
-    #if option == 1:
+    if option == 1:
+       option_1()
+
 
 
 def option_1():
-    pass
+    #setting backgroud white and text to black
+    print(Back.WHITE + Fore.BLACK + "In this example I am using a white background with black text")
+    
+    #resetting colors
+    print(Style.RESET_ALL + "By using Style.RESET_ALL I am now back to normal text")
+  
+    print(" ")
+    print(Back.YELLOW + Fore.BLACK + "Colorama might actually be a useful tool for learning code")
+    
+    print("")
+    print(Style.RESET_ALL + "I'll do an example below")
+    
+    
 
+
+
+start_colorama()
