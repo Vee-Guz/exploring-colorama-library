3498ae5c550f4a4ba935fc883f38043a23ad3a77
Vee-Guz <vero.guzman48612@gmail.com>
2020-12-11 21:07:19 -0800

    Wrote function that will deal with getting input number from user

---
+++ colorama_proj.py
@@ -0,0 +1,17 @@
+
+# for this project, there will be options that will allow user to view different possible ways that colorama can be used.
+
+def start_colorama():
+    # option variable will store a number (1 or 2) that will represent the option that will be printed out
+    option = int(intput("Please input 1 or 2? "))
+
+
+    # option 1 is an example of using colorama with text
+    # option 2 is an example of using colorama in an artistic form
+
+    #if option == 1:
+
+
+def option_1():
+    pass
+
